1cbaadc8555b964873b1aba1f6dfc6b1163c04dc
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-17 09:46:35 +0530

    added: catch error from api or url using errorElement

--- src/features/ui/AppLayout.jsx
+++ src/features/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
+import { useNavigation } from 'react-router-dom';
+
 import Header from './Header';
 import Main from './Main';
 import CartOverview from '../cart/CartOverview';
+import Loader from './Loader';
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
+
       <Header />
       <Main />
       <CartOverview />
