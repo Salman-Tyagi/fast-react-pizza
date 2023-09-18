0ce4e69c73e494836c6611e44bc800c74f36289c
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-18 13:33:31 +0530

    added redux tookit for user global state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Home from './features/ui/Home';
+import Home from './ui/Home';
+import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
-import AppLayout from './features/ui/AppLayout';
-import Error from './features/ui/Error';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
+
     children: [
       {
         path: '/',
@@ -23,13 +26,11 @@ const router = createBrowserRouter([
         loader: menuLoader,
         errorElement: <Error />,
       },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
+      { path: '/cart', element: <Cart /> },
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
@@ -41,6 +42,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-export default function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
