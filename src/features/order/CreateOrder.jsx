d415a9cbbbce4f4a48c7500c54aee9b76ab4cb4f
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-19 17:45:59 +0530

    implemented asynchronous location fetching using async react thunk used as middle thunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import store from '../../store';
 import { clearCart, getCart, getTotalPrice } from '../cart/cartSlice';
 import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
+import EmptyCart from '../cart/EmptyCart';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = str =>
@@ -20,15 +22,20 @@ function CreateOrder() {
   const formErrors = useActionData();
   const username = useSelector(state => state.user.username);
   const cart = useSelector(getCart);
+  const dispatch = useDispatch();
 
   const totalCartPrice = useSelector(getTotalPrice);
   const totalPriorityPrice = withPriority
     ? totalCartPrice * 1.2
     : totalCartPrice;
 
+  if (!cart.length) return <EmptyCart />;
+
   return (
     <div className='px-4 py-6'>
       <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method='POST'>
